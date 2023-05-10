7bbec839010511518c52be4dbe3711d490593bb3
Joy <joy.atulya@gmail.com>
2023-05-10 06:21:39 +0100

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RandomNumberComponent } from './random-number/random-number.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomNumberComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
