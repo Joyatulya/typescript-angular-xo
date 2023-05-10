32aa395052b1fac32a3cb496928a1bd548eeed3e
Joy <joy.atulya@gmail.com>
2023-05-10 07:34:24 +0100

    Making tic-tac-toe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RandomNumberComponent } from './random-number/random-number.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RandomNumberComponent,
     NavbarComponent,
+    SquareComponent,
+    BoardComponent,
   ],
   imports: [
     BrowserModule,
